fef0250fce368f81962b5725a0eac7dd8570ee97
Ramesh56789 <lara.java.2023@gmail.com>
2023-09-19 16:59:50 +0530

    first commit

--- A.java
+++ A.java
@@ -3,5 +3,9 @@ class A
 	public static void main(String[] args) 
 	{
 		System.out.println("i am into Lara repo");
+		System.out.println("i am into Lara repo");
+		System.out.println("i am into Lara repo");
+		System.out.println("i am into Lara repo");
+		System.out.println("i am into Lara repo");
 	}
 }
